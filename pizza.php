740115847e6002ab7c53aaf55e89b429ffa60b67
Ryokuryuu <ecastrotorres@iesesteveterradas.cat>
2017-10-09 17:25:04 +0200

    primero

---
+++ pizza.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pizzería</title>
+  </head>
+  <body>
+	  <form method="post" action="$recogida">
+	  
+		Masa fina<input name="masa1" value="masa fina" type="checkbox"></br>
+		Masa clásica<input name="masa2" value="masa clásica" type="checkbox"></br>
+		Orégano<input name="oregano" value="orégano" type="checkbox"></br>
+		Queso<input name="queso" value="queso" type="checkbox"></br>
+		Jamón<input name="jamon" value="jamón" type="checkbox"></br>
+		Bacon<input name="bacon" value="bacon" type="checkbox"></br>
+		Piña<input name="pina" value="piña" type="checkbox"></br>
+		Atún<input name="atun" value="atún" type="checkbox"></br>
+		Olivas<input name="olivas" value="olivas" type="checkbox"></br>
+		<input value="Pedir" type="submit">
+	  </form>
+	   
+	  <p id="respuesta"></p>
+	  <?php
+		  $_POST[""]
+	  
+	  ?>
